6d7cc1d5dfb3902d2bd6c43dac49cedb469a3b8d
Jimmie Goode <jimgoo@Jimmies-MacBook-Pro.local>
2022-02-28 09:33:49 -0600

    Add brews

--- mac-brews.sh
+++ mac-brews.sh
@@ -4,6 +4,9 @@ set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # brew packages
-brew install htop tree wget
-
+brew install \
+     htop \
+     tree \
+     wget \
+     gh
 
